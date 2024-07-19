516b4753cb2cf593e2c9f133c6b63d0b6192831b
Augusto Veiga <augusto.veiga@klarna.com>
2024-07-19 02:12:17 +0200

    new changes in footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
--- src/components/Footer/PrimaryFooter.js
+++ src/components/Footer/PrimaryFooter.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import './PrimaryFooter.css';
-import logo from '../../assets/logo.svg'; // ou '../../assets/logo.png'
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Email, Phone, ArrowForward } from '@mui/icons-material';
 
 const PrimaryFooter = () => {
   return (
     <footer className="primary-footer">
       <div className="container">
         <div className="footer-content">
-          <div className="footer-section logo-section">
-            <img src={logo} alt="GuardTech Logo" className="footer-logo" />
-          </div>
           <div className="footer-section contact-follow">
             <h4>Contact Us & Follow Us</h4>
-            <p><EmailIcon /> contact@guardtech.com</p>
-            <p><PhoneIcon /> +1 234 567 890</p>
+            <p><Email /> contact@guardtech.com</p>
+            <p><Phone /> +1 234 567 890</p>
             <div className="social-links">
               <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-instagram"></i>
+                <i className="fab fa-instagram" aria-label="Instagram"></i>
               </a>
               <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin"></i>
+                <i className="fab fa-linkedin" aria-label="LinkedIn"></i>
               </a>
             </div>
           </div>
@@ -33,6 +27,7 @@ const PrimaryFooter = () => {
               <li><a href="/services">Services</a></li>
               <li><a href="/trainings">Trainings</a></li>
               <li><a href="/contact">Contact</a></li>
+              <li><a href="/blog">Blog</a></li>
             </ul>
           </div>
           <div className="footer-section newsletter">
@@ -43,9 +38,10 @@ const PrimaryFooter = () => {
                   type="email" 
                   placeholder="Sign up for our newsletter" 
                   className="newsletter-input" 
+                  aria-label="Newsletter Signup"
                 />
-                <button type="submit" className="newsletter-button">
-                  <ArrowForwardIcon />
+                <button type="submit" className="newsletter-button" aria-label="Submit Newsletter Signup">
+                  <ArrowForward />
                 </button>
               </div>
             </form>
